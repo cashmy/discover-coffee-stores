af1372514f2b4438c1228bdc7480fbdd078abc16
Cash Myers <cmyers880@gmail.com>
2023-01-07 16:37:21 -0600

    feat: Add Banner & Hero Image

---
+++ public/_document.js
@@ -0,0 +1,27 @@
+import Document, { Head, Html, Main, NextScript } from "next/document"
+
+class MyDocument extends Document {
+  //   static async getInitialProps(ctx) {
+  //     const initialProps = await Document.getInitialProps(ctx)
+  //     return { ...initialProps }
+  //   }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preload" href="/fonts/IBMPlexSans-Bold.ttf" as="font" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/IBMPlexSans-Regular.ttf" as="font" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/IBMPlexSans-SemiBold.ttf" as="font" crossOrigin="anonymous" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument;
